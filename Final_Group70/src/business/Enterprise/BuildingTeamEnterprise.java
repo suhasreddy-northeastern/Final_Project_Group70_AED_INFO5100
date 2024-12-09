aa0ac0ed0dbeb561f1e05d177dfa4201ec080d1a
suhasreddy-northeastern <baluvanahallyraman.s@northeastern.edu>
2024-12-09 00:40:36 -0500

    added Enterprise files

---
+++ Final_Group70/src/business/Enterprise/BuildingTeamEnterprise.java
@@ -0,0 +1,25 @@
+package business.Enterprise;
+
+import business.Role.ConciergeRole;
+import business.Role.LeasingTeam;
+import business.Role.ResidentRole;
+import business.Role.Role;
+import java.util.ArrayList;
+
+public class BuildingTeamEnterprise extends Enterprise {
+
+    public BuildingTeamEnterprise() {
+        super(Type.Building_Team.getValue());
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList<>();
+        roles.add(new LeasingTeam());
+        roles.add(new ConciergeRole());
+        roles.add(new ResidentRole());
+
+        return roles;
+    }
+
+}
---
+++ Final_Group70/src/business/Enterprise/Enterprise.java
@@ -0,0 +1,90 @@
+package business.Enterprise;
+
+import business.Enterprise.EnterpriseDirectory;
+import business.Organisation.OrganisationDirectory;
+import business.Role.Role;
+import business.UserAccount.UserAccountDirectory;
+import business.WorkQueue.WorkQueue;
+import java.util.ArrayList;
+
+public abstract class Enterprise {
+
+    private String name;
+    private WorkQueue workQueue;
+    private OrganisationDirectory organisationDirectory;
+    private int EnterpriseID;
+    private static int counter;
+
+    public enum Type {
+        Admin("Admin Enterprise") {
+
+        },
+        Building_Team("Building Enterprise") {
+
+        },
+        Pest_Control_Organisation("Pest Enterprise") {
+
+        },
+        Maintenance_Team("Maintainence Enterprise") {
+
+        },
+        Packaging_Team("Packaging Enterprise") {
+
+        };
+        private String value;
+
+        private Type(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+    }
+
+    public Enterprise(String name) {
+        this.name = name;
+        workQueue = new WorkQueue();
+//        EnterpriseDirectory = new EnterpriseDirectory();
+//        userAccountDirectory = new UserAccountDirectory();
+        organisationDirectory = new OrganisationDirectory();
+        EnterpriseID = counter;
+        ++counter;
+    }
+
+    public abstract ArrayList<Role> getSupportedRole();
+
+//    public UserAccountDirectory getUserAccountDirectory() {
+//        return userAccountDirectory;
+//    }
+    public int getEnterpriseID() {
+        return EnterpriseID;
+    }
+
+    public OrganisationDirectory getOrganisationDirectory() {
+        return organisationDirectory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public WorkQueue getWorkQueue() {
+        return workQueue;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWorkQueue(WorkQueue workQueue) {
+        this.workQueue = workQueue;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
